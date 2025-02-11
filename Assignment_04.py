376d10a9c81d5ac21e297bae85edadd9ac66b256
Z0rian <zorian@nerol.net>
2025-02-11 13:13:08 -0500

    Added Assignment 04

---
+++ Assignment_04.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 31 12:23:14 2025
+
+@author: zorian
+"""
+
+#Hope this works!
+#Try num 2
+
+#First commit on new Asus laptop
